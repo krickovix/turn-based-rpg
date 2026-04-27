91f2591d3dd394071c2fbf55600e1c70b5a03f17
krickovicx <krickovic.milica@gmail.com>
2026-04-27 14:42:25 +0200

    added game_state global script
    ensured fetching the new configuration only happens once at the beginning of the run

---
+++ client/nordeus-turn-rpg/autoloads/game_state.gd
@@ -0,0 +1,19 @@
+extends Node
+
+var run_config: Dictionary = {}
+
+var run_active: bool = false
+
+var level: int = 0
+
+
+func start_new_run(config: Dictionary) -> void:
+	run_config = config
+	run_active = true
+	level = 1
+
+
+func end_run() -> void:
+	run_config = {}
+	run_active = false
+	level = 0
--- client/nordeus-turn-rpg/map/map.gd
+++ client/nordeus-turn-rpg/map/map.gd
@@ -1,25 +1,11 @@
 extends Node2D
 
 const BATTLE_PATH = "res://battle/battle.tscn"
-const RUN_CONFIG_REQ_URL = "http://localhost:8000/run/new"
-
-
-func _ready() -> void:
-	request_run_config()
 	
+func _ready() -> void:
+	print("Map opened, run config: ", GameState.run_config)
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file(BATTLE_PATH)
 	
 	
-func request_run_config() -> void:
-	var http = HTTPRequest.new()
-	print("SENT")
-	add_child(http)
-	http.request_completed.connect(_on_response)
-	http.request(RUN_CONFIG_REQ_URL)
-
-
-func _on_response(_result, _code, _headers, body) -> void:
-	print("HERE")
-	print(body.get_string_from_utf8())
