213c349f8fcf699a335ecf4784a47239e97c22d1
krickovicx <krickovic.milica@gmail.com>
2026-05-03 04:00:10 +0200

    Add Icon Button class

---
+++ client/nordeus-turn-rpg/components/icon_button/icon_button.gd
@@ -0,0 +1,18 @@
+class_name IconButton
+extends Button
+
+@onready var moveset_manager_icon: TextureRect = $MovesetManagerIcon
+@onready var moveset_manager_shadow: TextureRect = $MovesetManagerShadow
+@onready var label: Label = $Label
+
+
+func set_icon(text: String, texture_path: String) -> void:
+	moveset_manager_icon.texture = load(texture_path)
+	moveset_manager_shadow.texture = load(texture_path)
+	label.text = text
+
+func _on_mouse_entered() -> void:
+	moveset_manager_icon.position += Vector2(-10, -5)
+
+func _on_mouse_exited() -> void:
+	moveset_manager_icon.position -= Vector2(-10, -5)
