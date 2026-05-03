9b8ff9d53182e1ef307d8c571efaca59fcd08154
krickovicx <krickovic.milica@gmail.com>
2026-05-03 20:12:30 +0200

    Update MusicManager

--- client/nordeus-turn-rpg/autoloads/music_manager/music_manager.gd
+++ client/nordeus-turn-rpg/autoloads/music_manager/music_manager.gd
@@ -6,8 +6,6 @@ const MAP_MUSIC = preload("uid://cg0d5mr1rbbqb")
 @onready var player: AudioStreamPlayer = $AudioStreamPlayer
 
 var _current_track: AudioStream
-var _saved_position: float = 0.0
-var _saved_track: AudioStream
 
 
 func play_track(stream: AudioStream) -> void:
@@ -19,18 +17,7 @@ func play_track(stream: AudioStream) -> void:
 	player.play()
 
 func play_map_music() -> void:
-	if _saved_track == MAP_MUSIC and _saved_position > 0.0:
-		_current_track = MAP_MUSIC
-		player.stream = MAP_MUSIC
-		player.play(_saved_position)
-		_saved_position = 0.0
-		_saved_track = null
-	else:
-		play_track(MAP_MUSIC)
+	play_track(MAP_MUSIC)
 
 func play_battle_music() -> void:
-	if _current_track == MAP_MUSIC and player.playing:
-		_saved_track = MAP_MUSIC
-		_saved_position = player.get_playback_position()
-	
 	play_track(BATTLE_MUSIC)
