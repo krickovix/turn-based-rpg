2e29edf94f39f8d13abf2db58370a716915f3665
krickovicx <krickovic.milica@gmail.com>
2026-05-03 04:05:07 +0200

    Update post battle screen UI

--- client/nordeus-turn-rpg/battle/post_battle_screen/post_battle_screen.gd
+++ client/nordeus-turn-rpg/battle/post_battle_screen/post_battle_screen.gd
@@ -5,8 +5,6 @@ const MAP_PATH = "res://map/map.tscn"
 const END_STR = ["Victory!", "You died."]
 
 @onready var header_label: Label = $LabelContainer/HeaderLabel
-@onready var xp_label: Label = $LabelContainer/XPLabel
-@onready var xp_progress_bar: ProgressBar = $LabelContainer/XPProgressBar
 @onready var level_up_container: VBoxContainer = $LabelContainer/LevelUpContainer
 @onready var level_up_label: Label = $LabelContainer/LevelUpContainer/LevelUpLabel
 @onready var stat_gains_label: Label = $LabelContainer/LevelUpContainer/StatGainsLabel
@@ -18,7 +16,6 @@ const END_STR = ["Victory!", "You died."]
 func _ready() -> void:
 	visible = false
 	level_up_container.visible = false
-	xp_label.text = ""
 	learned_move_label.text = ""
 	learned_move_card.visible = false
 
@@ -26,13 +23,6 @@ func _on_battle_battle_over(xp_gained: int, levels_gained: int, move_learned: Mo
 	visible = true
 	
 	header_label.text = END_STR[0 if xp_gained else 1]
-	
-	xp_progress_bar.max_value = RunState.hero.level_threshold()
-	xp_progress_bar.value = RunState.hero.xp
-	
-	if xp_gained <= 0: return
-	
-	xp_label.text = "+%d XP" % xp_gained
 	
 	if levels_gained > 0:
 		level_up_container.visible = true
@@ -45,9 +35,9 @@ func _on_battle_battle_over(xp_gained: int, levels_gained: int, move_learned: Mo
 	if move_learned:
 		learned_move_label.text = "You learned:"
 		learned_move_card.set_move(move_learned)
-		learned_move_card.set_big()
+		learned_move_card.set_large()
 		learned_move_card.visible = true
-	else:
+	elif xp_gained > 0:
 		learned_move_label.text = "You already know all moves of this monster."
 
 func _on_back_button_pressed() -> void:
